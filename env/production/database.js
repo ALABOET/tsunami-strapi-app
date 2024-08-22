5c864af55f2a236fa5cc7a49bd5c526d2588ef4a
ALABOET <ityurin@alfabit.org>
2024-08-22 15:10:03 +0300

    fixing, adding

--- env/production/database.js
+++ env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'railway'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'password'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         ca: env('DATABASE_CA')
